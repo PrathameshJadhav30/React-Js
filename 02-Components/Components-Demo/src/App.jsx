87b69526c4d8e80426e58222061573217ffe3f89
Prathamesh <prathameshaj2004@gmail.com>
2026-02-27 21:42:39 +0530

    Components in React

---
+++ 02-Components/Components-Demo/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-Components/Components-Demo/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header style={{ backgroundColor: "#282c34", padding: "15px", color: "white" }}>
+      <h1>React Components Practical Demo</h1>
+      <h1>Prathamesh Jadhav</h1>
+    </header>
+  );
+}
+
+export default Header;
